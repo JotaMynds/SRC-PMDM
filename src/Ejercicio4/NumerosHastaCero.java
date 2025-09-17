d0724299aa1491ca4db215bf8c491384ab0a569f
JotaMynds <ivansanjuar@gmail.com>
2025-09-17 09:32:27 +0200

    Act 3 y 4 update

---
+++ src/Ejercicio4/NumerosHastaCero.java
@@ -0,0 +1,21 @@
+package Ejercicio4;
+
+import java.util.Scanner;
+
+public class NumerosHastaCero {
+    public static void NumerosHasta(){
+        Scanner sc = new Scanner(System.in);
+        int N = 0;
+        System.out.println("Introduce un número: ");
+        N =  sc.nextInt();
+
+        if(N>0){
+            while (N != 0) {
+                
+            }
+        }else{
+            System.out.println("Número no valido");
+        }
+
+    }
+}
