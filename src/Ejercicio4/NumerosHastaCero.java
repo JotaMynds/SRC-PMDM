fcd1c8f53f27ea10df213adb42bdb8cb2023413f
JotaMynds <ivansanjuar@gmail.com>
2025-09-17 12:56:21 +0200

    Update NumerosHastaCero.java

--- src/Ejercicio4/NumerosHastaCero.java
+++ src/Ejercicio4/NumerosHastaCero.java
@@ -3,88 +3,81 @@ package Ejercicio4;
 import java.util.Scanner;
 
 public class NumerosHastaCero {
+
     public static void NumerosHasta() {
         Scanner sc = new Scanner(System.in);
-        int N = 0;
-        System.out.println("Introduce un número: ");
-        int NM1 = 0;
-        int NM2 = 0;
-        int Nm = 0;
+        int N;
+
+        int mayor1 = 0;
+        int mayor2 = 0;
+        int menor = 0;
         boolean primero = true;
 
-        //Comprobar que es un numero y no un string y si no es negativo
-        while (!sc.hasNextBigInteger()) {
-            System.err.println("Formato incorrecto");
-            sc.next();
-            System.out.println("Introduce un número: ");
-            N = sc.nextInt();
-        }
+        System.out.println("Introduce un número positivo (0 para terminar): ");
 
-        N = sc.nextInt();
+        //Para validar que lo que se introduce es un número
+        while (true) {
+            if (sc.hasNextInt()) {
+                N = sc.nextInt();
+                break;
+            } else {
+                System.out.println("Formato incorrecto. Introduce un número entero:");
+                sc.next(); // limpiar entrada incorrecta
+            }
+        }
 
         while (N != 0) {
-            //Guardar variables para los números máximos y el número mínimo
-            while (N < 0) {
-                System.err.println("Número no válido");
-                sc.next();
-                System.out.println("Introduce un número positivo: ");
-                N = sc.nextInt();
-            }
-            if (primero) { // Hacer que el primer número sea el que
-                // iguala las asignaciones para que el mínimo pueda compararse
-                NM1 = N;
-                Nm = N;
-                primero = false;
-            }
-                //Guardar variables para los números máximos y el número mínimo
-
-                esPar(N); //Comprueba si es par con método externo
-
-                if (N > NM1) { // Si el número introducido es mayor que el mayor registrado lo sustituye
-                    NM2 = NM1;
-                    NM1 = N;
-                } else if (N > NM2 && N != NM1) { // Si el número introducido
-                    // es mayor que el 2 pero desigual del primero se le asigna
-                    NM2 = N;
-                }
-
-                // Si el numero introducido es menor que el menor registrado lo sustituye
-                if (N < Nm) {
-                    Nm = N;
-                }
-
-
-            System.out.println("Introduce otro número: ");
-
-            while (!sc.hasNextBigInteger()) {
-                System.err.println("Formato incorrecto");
-                sc.nextInt();
-                System.out.println("Introduce un número: ");
-                N = sc.nextInt();
-            }
-
-            N = sc.nextInt();
-
+            // Comprobar que el número sea positivo, si no se introduce de nuevo
             if (N < 0) {
-                System.err.println("Número no válido");
-                sc.nextInt();
-                System.out.println("Introduce un número positivo: ");
-                sc.nextInt();
+                System.out.println("Número no válido, introduce solo positivos:");
+            } else {
+                // Inicialización la primera vez
+                if (primero) {
+                    mayor1 = N;
+                    menor = N;
+                    primero = false;
+                }
+
+                // Comprobar par o impar
+                esPar(N);
+
+                // Actualizar mayores
+                if (N > mayor1) {
+                    mayor2 = mayor1;
+                    mayor1 = N;
+                } else if (N > mayor2 && N != mayor1) {
+                    mayor2 = N;
+                }
+
+                // Actualizar menor
+                if (N < menor) {
+                    menor = N;
+                }
             }
 
-        }
-        System.out.println("El mayor número es: " + NM1);
-        System.out.println("El segundo mayor número es: " + NM2);
-        System.out.println("El menor número es: " + Nm);
+            System.out.println("Introduce otro número (0 para terminar): ");
 
+            while (true) {
+                if (sc.hasNextInt()) {
+                    N = sc.nextInt();
+                    break;
+                } else {
+                    System.out.println("Formato incorrecto. Introduce un número entero:");
+                    sc.next(); // limpiar entrada incorrecta
+                }
+            }
+        }
+
+        if (!primero) { // Solo se muestra si se introdujeron números válidos
+            System.out.println("El mayor número es: " + mayor1);
+            System.out.println("El segundo mayor número es: " + mayor2);
+            System.out.println("El menor número es: " + menor);
+        } else {
+            System.out.println("No se introdujeron números válidos.");
+        }
     }
 
-    /**
-     * Cataloga si el número es par o impar
-     *
-     * @param N número introducido por el usuario
-     */
-
+    // Método independiente para catalogar números pares o impares
     public static void esPar(int N) {
         if (N % 2 == 0) {
             System.out.println("El número es par");
