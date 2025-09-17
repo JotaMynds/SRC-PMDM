04a85f217bc64d3f12e0ce2d33ca1aaa6e0a9923
JotaMynds <ivansanjuar@gmail.com>
2025-09-17 08:13:19 +0200

    Ejercicio 3 terminado y 1 arreglado

--- src/Ejercicio3/AdivinarNumero.java
+++ src/Ejercicio3/AdivinarNumero.java
@@ -4,26 +4,27 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class AdivinarNumero {
-    public static void main(String[] args) {
+    public static void NumeroRandom(){
         Scanner teclado = new Scanner(System.in);
+
         Random Aleatorio = new Random();
         int numero = Aleatorio.nextInt(1,100);
-        NumeroRandom(teclado, numero);
-    }
-    public static void NumeroRandom(Scanner teclado, int numero){
+
         System.out.println("Introduce un numero para adivinar el numero:");
         int N1 = teclado.nextInt();
-        while(numero!=N1){ //PONERLO CON FOR, SOLAMENTE 10 INTENTOS
-            System.out.println("Introduce un numero:");
+
+        for(int i= 1; i<=10;i++){
             if(N1<numero){
                 System.out.println("El numero es mayor a "+N1);
             }else if(N1>numero){
                 System.out.println("El numero es menor a "+N1);
             }else {
-
+                System.out.println("El numero " + N1 + " es el número correcto");
+                break;
             }
+            System.out.println("Introduce un numero:");
+            N1 = teclado.nextInt();
         }
-
     }
 
 
---
+++ src/Ejercicio3/Main.java
@@ -0,0 +1,12 @@
+package Ejercicio3;
+
+import Ejercicio3.AdivinarNumero;
+
+import Ejercicio3.AdivinarNumero;
+
+public class Main {
+    public static void main(String[] args) {
+        AdivinarNumero.NumeroRandom();
+
+    }
+}
